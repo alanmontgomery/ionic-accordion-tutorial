9952b804e744a700ade1067f986a9737d919b214
Alan Montgomery <alan.montgomery@tascomi.com>
2022-01-28 11:49:31 +0000

    Update to v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { setupIonicReact } from '@ionic/react';
 import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
@@ -22,6 +23,8 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import Topic from './pages/Topic';
+
+setupIonicReact({});
 
 const App = () => (
   <IonApp>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,60 @@
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { Accordion } from "../components/Accordion";
+import {IonAccordion, IonAccordionGroup, IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { Accordion } from '../components/Accordion';
 
 const Home = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Topics</IonTitle>
+          <IonTitle>Accordion</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Topics</IonTitle>
+            <IonTitle size="large">Accordion</IonTitle>
           </IonToolbar>
         </IonHeader>
 
         <Accordion />
+
+        {/* <IonAccordionGroup>
+          <IonAccordion>
+            <IonItem slot="header">
+              <IonLabel>Languages</IonLabel>
+            </IonItem>
+
+            <IonList slot="content">
+              <IonItem>
+                <IonLabel>English</IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel>Spanish</IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel>Italian</IonLabel>
+              </IonItem>
+            </IonList>
+          </IonAccordion>
+
+          <IonAccordion>
+            <IonItem slot="header">
+              <IonLabel>Languages 2</IonLabel>
+            </IonItem>
+
+            <IonList slot="content">
+              <IonItem>
+                <IonLabel>English</IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel>Spanish</IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel>Italian</IonLabel>
+              </IonItem>
+            </IonList>
+          </IonAccordion>
+        </IonAccordionGroup> */}
       </IonContent>
     </IonPage>
   );
